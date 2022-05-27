38c05856e25dbf49035429ede48c76c9005caa1c
smileypop <matthewlaird@gmail.com>
2022-05-27 18:46:44 +0900

    change query params to alphanumeric

--- Sources/Sahha/Configs/APIConfig.swift
+++ Sources/Sahha/Configs/APIConfig.swift
@@ -71,7 +71,7 @@ class ApiEndpoint {
         var urlPath = endpointPath.rawValue
         for (index, queryParam) in queryParams.enumerated() {
             // escape string
-            let escapedString = queryParam.value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? queryParam.value
+            let escapedString = queryParam.value.addingPercentEncoding(withAllowedCharacters: .alphanumerics) ?? queryParam.value
             if (index == 0) {
                 urlPath.append("?\(queryParam.key)=\(escapedString)")
             } else {
