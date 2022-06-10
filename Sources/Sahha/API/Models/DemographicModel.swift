1a006b80eb23b81fbadc190ad28de1cbf310b088
smileypop <matthewlaird@gmail.com>
2022-06-10 17:18:47 +0900

    update analyzation model

--- Sources/Sahha/API/Models/DemographicModel.swift
+++ Sources/Sahha/API/Models/DemographicModel.swift
@@ -7,8 +7,6 @@ public struct SahhaDemographic: Codable {
     public var gender: String?
     public var country: String?
     public var birthCountry: String?
-    /*
-     // Coming Soon
     public var ethnicity: String?
     public var occupation: String?
     public var industry: String?
@@ -17,12 +15,19 @@ public struct SahhaDemographic: Codable {
     public var relationship: String?
     public var locale: String?
     public var livingArrangement: String?
-     */
-    
-    public init(age: Int? = nil, gender: String? = nil, country: String? = nil, birthCountry: String? = nil) {
+
+    public init(age: Int? = nil, gender: String? = nil, country: String? = nil, birthCountry: String? = nil, ethnicity: String? = nil, occupation: String? = nil, industry: String? = nil, incomeRange: String? = nil, education: String? = nil, relationship: String? = nil, locale: String? = nil, livingArrangement: String? = nil) {
         self.age = age
         self.gender = gender
         self.country = country
         self.birthCountry = birthCountry
+        self.ethnicity = ethnicity
+        self.occupation = occupation
+        self.industry = industry
+        self.incomeRange = incomeRange
+        self.education = education
+        self.relationship = relationship
+        self.locale = locale
+        self.livingArrangement = livingArrangement
     }
 }
