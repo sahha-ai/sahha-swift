c0cc6eb34bb07c8246c862f629a2837ae2405f00
smileypop <matthewlaird@gmail.com>
2023-01-20 14:20:25 -0700

    lower minimum ios version to 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Sahha",
     platforms: [
-             .iOS(.v14),
+             .iOS(.v12),
          ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
--- Sources/Sahha/Configs/SahhaConfig.swift
+++ Sources/Sahha/Configs/SahhaConfig.swift
@@ -65,7 +65,7 @@ class SahhaConfig {
         }
     }
     
-    static let sdkVersion: String = "0.1.3"
+    static let sdkVersion: String = "0.1.4"
     
     static let appId: String = Bundle.main.bundleIdentifier ?? ""
             
