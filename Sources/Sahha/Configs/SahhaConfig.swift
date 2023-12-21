8945cc1226822d2f66216b2c3474f1ba9237f675
smileypop <matthewlaird@gmail.com>
2023-12-21 14:25:55 +0900

    bump version

--- Sources/Sahha/Configs/SahhaConfig.swift
+++ Sources/Sahha/Configs/SahhaConfig.swift
@@ -54,7 +54,7 @@ class SahhaConfig {
         }
     }
     
-    static let sdkVersion: String = "0.3.3"
+    static let sdkVersion: String = "0.3.4"
     
     static let appId: String = Bundle.main.bundleIdentifier ?? ""
             
