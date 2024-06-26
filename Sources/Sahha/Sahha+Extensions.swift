f9e730074100f89dbd999127e5d45d237ff8c18a
smileypop <matt@sahha.ai>
2024-06-26 21:33:47 +0900

    change how dates are saved to local storage

--- Sources/Sahha/Sahha+Extensions.swift
+++ Sources/Sahha/Sahha+Extensions.swift
@@ -6,7 +6,7 @@ import Foundation
 
 extension UserDefaults {
     func set(date: Date?, forKey key: String){
-        self.set(date, forKey: key)
+        self.setValue(date, forKey: key)
     }
     func date(forKey key: String) -> Date? {
         return self.value(forKey: key) as? Date
